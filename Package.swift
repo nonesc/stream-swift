560f839b23db1df78cd746a1458d37d49c3c66ce
Sujin <s.chaichanamongkol@accenture.com>
2024-03-17 15:19:15 +0700

    Update package file

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.0
+// swift-tools-version:5.7.1
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -6,19 +6,19 @@ import PackageDescription
 let package = Package(
     name: "GetStream",
     platforms: [
-        .iOS(.v11)
+        .iOS(.v14)
     ],
     products: [
         .library(name: "GetStream", targets: ["GetStream"]),
         .library(name: "Faye", targets: ["Faye"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/Moya/Moya.git", .upToNextMajor(from: "14.0.0")),
-        .package(url: "https://github.com/daltoniam/Starscream.git", .upToNextMajor(from: "4.0.0")),
-        .package(url: "https://github.com/sendyhalim/Swime", .upToNextMajor(from: "3.0.0")),
+        .package(url: "https://github.com/Moya/Moya.git", .upToNextMajor(from: "14.0.1")),
+        .package(url: "https://github.com/daltoniam/Starscream.git", .upToNextMajor(from: "4.0.4")),
+        .package(url: "https://github.com/camelan/Swime", .upToNextMajor(from: "3.1.1")),
     ],
     targets: [
-        .target(name: "GetStream", dependencies: ["Moya", "Faye", "Swime"], path: "Sources", exclude: ["Token"]),
+        .target(name: "GetStream", dependencies: ["Moya", "Faye", .product(name: "Swime", package: "Swime")], path: "Sources", exclude: ["Token"]),
         .target(name: "Faye", dependencies: ["Starscream"], path: "Faye"),
         .testTarget(name: "GetStreamTests", dependencies: ["GetStream"], path: "Tests", exclude: ["Token"]),
     ]
